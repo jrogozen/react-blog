9d8630119b9bfd7f3035c500b0b9c68487b8224a
Jon Rogozen <jon.rogozen@gmail.com>
2015-02-18 08:45:12 -0800

    add route middleware for posts with slug

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var Q = require('q');
 
 app.set('js', app.get('env') === 'development' ? 'dev' : 'min');
 
-app.use('/scripts/bundle.js', function(req, res, next) {
-  req.url = '/scripts/bundle.' + app.get('js') + '.js';
+app.use(function(req, res, next) {
+  if(req.url === '/scripts/bundle.js') {
+    req.url = '/scripts/bundle.' + app.get('js') + '.js';
+  }
   next();
-})
+});
 
 app.use('/api/posts', function(req, res, next) {
   Utils.getPosts()
@@ -17,11 +19,18 @@ app.use('/api/posts', function(req, res, next) {
       res.posts = data;
       next();
     });
-})
+});
+
+app.use(/\/api\/posts\/\S+/, function(req, res, next) {
+});
 
 app.get('/api/posts', function(req, res) {
   res.json(res.posts);
-})
+});
+
+app.get('/api/posts/:slug', function(req, res) {
+
+});
 
 app.use(express.static('./public'));
 
