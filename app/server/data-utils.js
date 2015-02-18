b2f4c2238b6e08db11cd345200b43067b3ed9ed1
Jon Rogozen <jon.rogozen@gmail.com>
2015-02-17 17:03:57 -0800

    titleize function

--- app/server/data-utils.js
+++ app/server/data-utils.js
@@ -2,7 +2,11 @@ var fs = require('fs');
 var marked = require('marked');
 
 function titleize(fileName) {
+  return fileName.replace(/_/gi, '/').substring(0, fileName.length - 3);
+}
 
+function titleToFilename(title) {
+  title.replace(/\//gi, '_').concat('.md');
 }
 
 function parsePosts() {
@@ -20,7 +24,8 @@ function parsePosts() {
       fs.readFile(dir + '/' + file, 'utf-8', function(err, fileData) {
         if (err) throw err;
 
-        data[file] = marked(fileData);
+        var fileTitle = titleize(file);
+        data[fileTitle] = marked(fileData);
 
         if (0 === --i) {
           console.log(data);
@@ -28,10 +33,6 @@ function parsePosts() {
       });
     });
   });
-}
-
-function parsePost(year, month, day, title) {
-
 }
 
 exports.getPosts = function() {
