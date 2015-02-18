292e6e2aa3a74de181bab9f950549e3b913569c4
Jon Rogozen <jon.rogozen@gmail.com>
2015-02-18 15:34:07 -0800

    add click handler to view new posts

--- app/component/react-blog.js
+++ app/component/react-blog.js
@@ -5,28 +5,34 @@ var $ = require('jquery-browserify');
 var ReactBlog = React.createClass({
   getInitialState: function() {
     return {
-      posts: []
+      posts: [],
     };
   },
   componentDidMount: function() {
     $.get(this.props.url, function(data) {
       if (this.isMounted()) {
         this.setState({
-          posts: data
+          posts: data,
+          post: data[0]
         });
       }
     }.bind(this));
   },
+  focusPost: function(slug) {
+    $.get('/api/posts/' + slug, function(data) {
+      this.setState({
+        post: data
+      })
+    }.bind(this));
+  },
   render: function() {
-    var latestPost = this.state.posts[0];
-
     return (
       <div className="layout">
         <div className="layout layout-sidebar">
-          <PostList posts={this.state.posts}/>
+          <PostList handleTitleClick={this.focusPost} posts={this.state.posts}/>
         </div>
         <div className="layout layout-content">
-          <PostViewer post={latestPost}/>
+          <PostViewer post={this.state.post}/>
         </div>
       </div>
     )
@@ -34,11 +40,14 @@ var ReactBlog = React.createClass({
 });
 
 var PostList = React.createClass({
+  handleTitleClick: function(slug) {
+    this.props.handleTitleClick(slug);
+  },
   render: function() {
     var posts = this.props.posts;
 
     var postSnippets = posts.map(function(post, i) {
-      return <PostSnippet data={post} key={i} />;
+      return <PostSnippet data={post} key={i} handleTitleClick={this.handleTitleClick}/>;
     }, this);
 
     return (
@@ -52,11 +61,14 @@ var PostList = React.createClass({
 });
 
 var PostSnippet = React.createClass({
+  handleTitleClick: function(slug) {
+    this.props.handleTitleClick(slug);
+  },
   render: function() {
     var post = this.props.data;
     return (
       <li>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.handleTitleClick.bind(this, post.slug)}>{post.title}</h1>
       </li>
     )
   }
@@ -71,9 +83,11 @@ var PostViewer = React.createClass({
   render: function() {
     /* handle check for initial load which doesn't include prop data yet */
     if (this.props.post) {
+      var post = this.props.post;
       return (
         <div>
-          {this.props.post.title}
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.content}} />
         </div>
       )
     }
@@ -81,6 +95,6 @@ var PostViewer = React.createClass({
       <div/>
     )
   }
-})
+});
 
 module.exports = ReactBlog;
